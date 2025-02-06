b8c0d74d2b916c9fa243d87aa7fe72c6b5396a5f
nashrulmalik <nashrulmalik@gmail.com>
2025-02-06 13:26:06 +0700

    regular update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',             // Important for GitHub Pages deployment
+  basePath: '/next-static',     // Replace with your repository name
+  assetPrefix: '/next-static/', // Important for assets
+  images: {
+    unoptimized: true,          // Required for static export of images
+  },
 };
 
 export default nextConfig;
